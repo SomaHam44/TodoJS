e377fb3e10e055827406f61b62ca2339ebea947e
Solti Soma <somasolti@gmail.com>
2021-10-12 15:50:42 +0200

    JS fájl elkezdése

---
+++ main.js
@@ -0,0 +1,9 @@
+function kattintas() {
+
+
+}
+
+function init() {
+    document.getElementById("listakeszitoGomb").addEventListener("click", kattintas);
+}
+document.addEventListener("DOMContentLoaded", init);
