30809350ae8deb25d3a4195d25aea178ebb4468b
Solti Soma <somasolti@gmail.com>
2021-10-12 17:50:36 +0200

    Bővítés

--- main.js
+++ main.js
@@ -1,16 +1,24 @@
+
 function kattintas() {
+    
     document.getElementById("beviteliMezo").innerHTML = "";
 
     let mezo = document.getElementById("beviteliMezo").value;
     let lista = document.createElement("ul");
     let li = document.createElement("li");
-    li.innerHTML = mezo;
+    let jeloloNegyzet = "<input type='checkbox'>";
+    let x = "X";
+    li.innerHTML = jeloloNegyzet + mezo +  " " + x;
     lista.appendChild(li);
     document.getElementById("lista").appendChild(lista);
+    
+    
+
 
 }
 
 function init() {
     document.getElementById("listakeszitoGomb").addEventListener("click", kattintas);
+    
 }
 document.addEventListener("DOMContentLoaded", init);
