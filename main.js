13f2c23f5d62762ba0d53fe535b21889680dbc12
SomaHam44 <somasolti@gmail.com>
2021-10-13 23:24:06 +0200

    Kattintás után töröl

--- main.js
+++ main.js
@@ -6,10 +6,15 @@ function kattintas() {
     let mezo = document.getElementById("beviteliMezo").value;
     let lista = document.createElement("ul");
     let li = document.createElement("li");
-    let jeloloNegyzet = "<input type='checkbox'>";
-    let x = "X";
-    li.innerHTML = jeloloNegyzet + mezo +  " " + x;
     lista.appendChild(li);
+    let jeloloNegyzet = document.createElement("input");
+    jeloloNegyzet.setAttribute("type", "checkbox");
+    li.appendChild(jeloloNegyzet);
+    let x = document.createElement("span");
+    li.innerHTML = jeloloNegyzet + mezo +  " " + x.innerHTML;
+    x.innerHTML = "X";
+    li.appendChild(x);
+    x.addEventListener("click", () => {li.innerHTML = " "});
     document.getElementById("lista").appendChild(lista);
     
     
